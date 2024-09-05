c077d60f476bbb82ed179c7051800858c579144e
Siddharth-1698 <66638045+Siddharth-1698@users.noreply.github.com>
2024-09-05 18:02:19 +0530

    fix: Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open("README.md", "r") as fh:
 
 setup(
     name="neo-pusher",  # Your package name
-    # use_scm_version=True,  # Use setuptools-scm to handle versioning
-    version = "1.1.3",
-    # use_scm_version={"local_scheme": "no-local-version"},
+    use_scm_version=True,  # Use setuptools-scm to handle versioning
+    # version = "1.1.3",
+    use_scm_version={"local_scheme": "no-local-version"},
     # Avoid using local versions
     setup_requires=[
         "setuptools>=42",
